6c1628861dc7efdc9a68f136cfaffd58b4b5da4c
Ben Sparks <bensparks0@gmail.com>
2021-04-04 23:41:13 +0200

    Cleaned up Curve contract - view skeleton and calcIntegral

--- Bonding Curve/contracts/Curve.sol
+++ Bonding Curve/contracts/Curve.sol
@@ -2,73 +2,42 @@
 pragma solidity ^0.7.0;
 
 import "./Token.sol";
+import "./CollateralToken";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Curve {
-    Token internal token;
-    IERC20 internal collateral;
+    IERC20 public collateralToken;
+    Token public bondedToken;
 
-    constructor(address _token, address _collateral) {
-        token = Token(_token);
-        collateral = IERC20(_collateral);
+    constructor(address _collateralToken, address _bondedToken) {
+        collateralToken = IERC20(_collateralToken);
+        bondedToken = Token(_bondedToken);
     }
 
-    function buyPrice(uint256 _amount) public view returns(uint256) {
-        return curveIntegral(token.totalSupply(), token.totalSupply() + _amount);
+    function buyPrice(uint256 _amount) public view returns (uint256) {
+        return 1; //TODO
     }
 
-    function sellReward(uint256 _amount) public view returns(uint256) {
-        return curveIntegral(token.totalSupply() - _amount, token.totalSupply());
+    function sellReward(uint256 _amount) public view returns (uint256) {
+        return 1; //TODO
     }
 
-    function collateralToken() public view returns(address) {
-        return address(collateral);
+    function collateralToken() public view returns (address) {
+        return address(collateralToken);
     }
 
-    function bondedToken() public view returns(address) {
-        return address(token);
+    function bondedToken() public view returns (address) {
+        return address(bondedToken);
     }
 
-    function mint(uint256 _amount) public returns(bool){
-        uint256 cost = buyPrice(_amount);
-        
-        require(
-            collateral.allowance(msg.sender, address(this)) >= cost,
-            "User has not approved"
-        );
-        require(
-            collateral.transferFrom(
-                msg.sender,
-                address(this),
-                cost
-            ),
-            "Transfer of collateral failed"
-        );
+    function calcIntegral(uint256 a, uint256 b) internal returns (uint256) {
+        require(a <= b, "a must be <= b");
 
-        token.mint( _amount, msg.sender);
-        
-        return true;
-    }
-
-    function burn(uint256 _amount) public returns(bool) {
-        uint256 reward = sellReward(_amount);
-
-        token.burn(_amount, msg.sender);
-
-        require(
-            collateral.transfer(
-                msg.sender,
-                reward
-            ),
-            "Transfer of collateral failed"
-        );
-    }
-
-    function curveIntegral(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > a, "Math breaks :/");
-
-        uint256 answer = (b**2 - a**2) + 40*(b - a);
-
-        return answer/200;
+        if (a == b) {
+            return 0;
+        } else {
+            uint256 res = (b**2 - a**2) + 40 * (b - a);
+            return res / 200; //ASK - why not do this in 1 line?
+        }
     }
 }
